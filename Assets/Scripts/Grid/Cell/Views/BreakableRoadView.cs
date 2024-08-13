1caf2f62552075b2d1b80b828e8b029f2aac24df
Alessandro Simeoni <alessandrosimeoni97@gmail.com>
2024-08-13 10:50:24 +0200

    G_RoadCracks visual phases

--- Assets/Scripts/Grid/Cell/Views/BreakableRoadView.cs
+++ Assets/Scripts/Grid/Cell/Views/BreakableRoadView.cs
@@ -1,18 +1,36 @@
 using Architecture;
 using Cysharp.Threading.Tasks;
+using System;
 using UnityEngine;
+using Utility;
 
 namespace Grid.Cell
 {
     public class BreakableRoadView : AView
     {
-        [SerializeField] private GameObject roadGraphic = null;
+        [SerializeField] private MeshRenderer roadGraphic = null;
+        [SerializeField] private Material[] cellPhaseMaterial = Array.Empty<Material>();
         [SerializeField] private GameObject emptyGraphic = null;
+        [SerializeField] private AudioClip damageAudioClip = null;
+        [SerializeField] private ParticleSystem roadToGrassParticle= null;
 
         public override async UniTask ChangeView()
         {
+            if (roadToGrassParticle != null)
+            {
+                roadToGrassParticle.Play();
+                await UniTask.WaitForSeconds(roadToGrassParticle.totalTime / 2);
+            }
             emptyGraphic.SetActive(true);
-            roadGraphic.SetActive(false);
+            roadGraphic.gameObject.SetActive(false);
+        }
+
+        public async UniTask DamageCell(int cellCurrentHealth)
+        {
+            if ( damageAudioClip != null)
+                AudioManager.instance.PlayAudioClipPreDefinedSource(damageAudioClip, roadGraphic.GetComponent<AudioSource>());
+            
+            roadGraphic.material = cellPhaseMaterial[cellCurrentHealth-1];
             await UniTask.NextFrame();
         }
     }
