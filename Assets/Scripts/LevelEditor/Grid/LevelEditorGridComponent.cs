4538d4bf1868172cdd12b514f40af5a389e6c305
Alessandro Simeoni <alessandrosimeoni97@gmail.com>
2024-10-11 13:56:16 +0200

    level editor: levels buttons disabled in simulation + fixes

--- Assets/Scripts/LevelEditor/Grid/LevelEditorGridComponent.cs
+++ Assets/Scripts/LevelEditor/Grid/LevelEditorGridComponent.cs
@@ -100,6 +100,8 @@ namespace LevelEditor
                 if (c.GetType() == typeof(ShieldedCityCell))
                     shieldedCityCell = (ShieldedCityCell)c;
             }
+
+            EvaluateSimulationCondition();
         }
 
         public async UniTask PositionCell(BaseCell newCell, BaseCell toBeReplacedCell)
@@ -149,9 +151,14 @@ namespace LevelEditor
 
             if (emptyCellsCounter == maxEmptyCellNumber)
                 TurnOffVisualCells();
-            
-            OnSimulationCondition?.Invoke(emptyCellsCounter == 0 && startCell != null && exitCell != null && cityQuantity > 0 && ((generatorCell == null && shieldedCityCell == null) || (generatorCell != null && shieldedCityCell != null)));
+
+            EvaluateSimulationCondition();
             OnCellPositioned?.Invoke();
+        }
+
+        private void EvaluateSimulationCondition()
+        {
+            OnSimulationCondition?.Invoke(emptyCellsCounter == 0 && startCell != null && exitCell != null && cityQuantity > 0 && ((generatorCell == null && shieldedCityCell == null) || (generatorCell != null && shieldedCityCell != null)));
         }
 
         public Vector2Int GetCellIndexes(BaseCell targetCell)
@@ -195,6 +202,16 @@ namespace LevelEditor
                     visualCell[i,j].ToggleGrayBox(false);
                     visualCell[i,j].ToggleSelectCubes(false);
                 }
+        }
+
+        public void EnableLevelButtonsGrayBox()
+        {
+            TurnOffVisualCells();
+
+            for (int i = 0; i < width; i++)
+                for (int j = 0; j < height; j++)
+                    if (Array.IndexOf<BaseCell>(levelButtonArray, gridArray[i, j]) != -1)
+                        visualCell[i, j].ToggleGrayBox(true);
         }
 
         [ContextMenu("Test EnvironmentCellSelected")]
